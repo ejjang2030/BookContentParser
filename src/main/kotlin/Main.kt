319b4b4be7595fb28a08b5ec13c226275ed3c7d4
ejjang2030 <ejjang2030@gmail.com>
2022-12-14 22:30:11 +0900

    it can be typed what you want to search a keyword

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,29 +3,35 @@ import java.util.*
 import javax.swing.text.AbstractDocument.Content
 
 fun main(args: Array<String>) {
-    val jsonArray = BookContentParser.searchBook(SecretId.CLIENT_ID,
+    val scanner = Scanner(System.`in`)
+    print("찾고자 하는 책 제목을 입력하세요 : ")
+    var title = scanner.next()
+    print("원하는 책 검색량을 입력하세요 : ")
+    var count = scanner.nextInt()
+    var jsonArray = BookContentParser.searchBook(SecretId.CLIENT_ID,
         SecretId.CLIENT_ID_SECRET,
         BookContentParser.DataType.JSON,
-        "자바")
+        title,
+        display = count)
     println(jsonArray)
     for(i in 0 until jsonArray.size()) {
         println("$i 번 : ${jsonArray[i]}")
     }
 
-    val scanner: Scanner = Scanner(System.`in`)
     print("번호를 입력하세요 : ")
-    val number = scanner.nextInt()
+    var number = scanner.nextInt()
+    scanner.reset()
     println("제목 : ${BookContentParser.getTitle(jsonArray, number)}")
     println("목차 ----------------------\n")
-    val list = BookContentParser.getContentToList(jsonArray, number)
-    for(l in list) {
-        println(l)
-    }
-    println(list)
+    var list = BookContentParser.getContentToList(jsonArray, number)
+//    for(l in list) {
+//        println(l)
+//    }
+//    println(list)
 
-    println("Tokenized : ${ContentTreeParser(list).getTree()}")
+//    println("Tokenized : ${ContentTreeParser(list).getTree()}")
 
-    val (tokenSet, items) = ContentTreeParser(list).getTree()
+    var (tokenSet, items) = ContentTreeParser(list).getTree()
 
     for(item in items.withIndex()) {
         for(token in tokenSet.withIndex()) {
