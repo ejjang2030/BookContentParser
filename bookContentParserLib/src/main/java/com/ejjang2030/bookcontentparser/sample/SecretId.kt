c8211b4fd7d02079219e38de605ee378b107df2d
ejjang2030 <ejjang2030@gmail.com>
2022-12-19 23:15:17 +0900

    deleted : sample codes

--- bookContentParserLib/src/main/java/com/ejjang2030/bookcontentparser/sample/SecretId.kt
+++
@@ -1,7 +0,0 @@
-package com.ejjang2030.bookcontentparser.sample
-
-object SecretId {
-    val NAVER_CLIENT_ID = "naverClientId"
-    val NAVER_CLIENT_ID_SECRET = "naverClientIdSecret"
-    val KAKAO_REST_API_KEY = "kakaoRestApiKey"
-}
