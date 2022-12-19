133748f57a1c7a0bfbe354e406174a438e3132fc
ejjang2030 <ejjang2030@gmail.com>
2022-12-19 23:12:39 +0900

    feat(SecretId) : created sample SecretId object

---
+++ bookContentParserLib/src/main/java/com/ejjang2030/bookcontentparser/sample/SecretId.kt
@@ -0,0 +1,7 @@
+package com.ejjang2030.bookcontentparser.sample
+
+object SecretId {
+    val NAVER_CLIENT_ID = "naverClientId"
+    val NAVER_CLIENT_ID_SECRET = "naverClientIdSecret"
+    val KAKAO_REST_API_KEY = "kakaoRestApiKey"
+}
