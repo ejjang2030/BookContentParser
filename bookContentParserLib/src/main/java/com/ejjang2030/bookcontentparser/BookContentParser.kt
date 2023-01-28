735bf0b36bee32baba4d21caf89667ef20e80e24
ejjang2030 <ejjang2030@gmail.com>
2023-01-29 04:02:02 +0900

    fix : 불필요한 코드 제거 -> getContent메서드

--- bookContentParserLib/src/main/java/com/ejjang2030/bookcontentparser/BookContentParser.kt
+++ bookContentParserLib/src/main/java/com/ejjang2030/bookcontentparser/BookContentParser.kt
@@ -11,31 +11,9 @@ object BookContentParser {
             .replace("&amp;", "&")
             .replace("<b>", "")
             .replace("</b>", "")
+
             .split("<br>")
             .filter { it != "" }
             .toMutableList()
-    }
-
-    fun getContent(jsonArray: JsonArray, index: Int): String {
-        val book = jsonArray.get(index).asJsonObject
-        val link = book.get("link").toString().replace("\"", "")
-        var html = Jsoup.connect(link)
-            .method(Connection.Method.GET)
-            .ignoreContentType(true)
-            .execute().parse().select("div[class=infoItem_data_text__bUgVI]")
-        var content: String = ""
-        for(i in 0 until html.size) {
-            println("$i 번째 : ${html.get(i).html()}")
-            if(html.get(i).html().contains("<br>")) {
-                content = html.get(i).html().toString()
-            }
-        }
-        println()
-        println()
-        content = content.replace("&amp;", "&")
-            .replace("<b>", "")
-            .replace("</b>", "")
-
-        return content
     }
 }
